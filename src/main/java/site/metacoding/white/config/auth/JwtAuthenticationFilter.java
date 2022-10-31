7db2474f22d466b6ae1daf74d7668bf71216bdae
gitthathonor <jeong.suyoung92@gmail.com>
2022-10-31 14:53:11 +0900

    jwt 로그인 시 토큰 생성완료

--- src/main/java/site/metacoding/white/config/auth/JwtAuthenticationFilter.java
+++ src/main/java/site/metacoding/white/config/auth/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package site.metacoding.white.config.auth;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 import java.util.Optional;
 
 import javax.servlet.Filter;
@@ -12,6 +13,8 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -20,29 +23,32 @@ import lombok.extern.slf4j.Slf4j;
 import site.metacoding.white.domain.User;
 import site.metacoding.white.domain.UserRepository;
 import site.metacoding.white.dto.ResponseDto;
+import site.metacoding.white.dto.SessionUser;
 import site.metacoding.white.dto.UserReqDto.LoginReqDto;
 import site.metacoding.white.util.SHA256;
 
-@RequiredArgsConstructor
 @Slf4j
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter implements Filter {
 
-    private final UserRepository userRepository; // DI (FilterConfig에게서 주입받음)
+    private final UserRepository userRepository; // DI (FilterConfig 주입받음)
 
-    // "/login" 요청시
+    // /login 요청시
     // post 요청시
-    // username, password(json)
+    // username, password (json)
     // db확인
     // 토큰 생성
-
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
 
-        // POST요청이 아닌 것을 거부
-        denyNotPostRequest(req, resp);
+        // Post요청이 아닌것을 거부
+        if (!req.getMethod().equals("POST")) {
+            customResponse("로그인시에는 post요청을 해야 합니다.", resp);
+            return;
+        }
 
         // Body 값 받기
         ObjectMapper om = new ObjectMapper();
@@ -50,53 +56,61 @@ public class JwtAuthenticationFilter implements Filter {
         log.debug("디버그 : " + loginReqDto.getUsername());
         log.debug("디버그 : " + loginReqDto.getPassword());
 
-        User userPS = userRepository.findByUsername(loginReqDto.getUsername());
-        if (userPS == null) {
-            // findByUsername 나중에 처리하기
-            resp.setContentType("application/json; charset=utf-8");
-            PrintWriter out = resp.getWriter();
-            resp.setStatus(400);
-            ResponseDto<?> responseDto = new ResponseDto<>(-1, "유저네임이 없습니다", null);
-            ObjectMapper om2 = new ObjectMapper();
-            String body = om2.writeValueAsString(responseDto);
-            out.println(body);
-            out.flush();
+        // 유저네임 있는지 체크
+        Optional<User> userOP = userRepository.findByUsername(loginReqDto.getUsername());
+        if (userOP.isEmpty()) {
+            customResponse("유저네임을 찾을 수 없습니다.", resp);
             return;
-        } else {
-            SHA256 sh = new SHA256();
-            String encPassword = sh.encrypt(loginReqDto.getPassword());
-            if (userPS.getPassword().equals(encPassword)) {
-                // JWT 토큰 만들고 응답하기
-            } else {
-                // findByUsername 나중에 처리하기
-                resp.setContentType("application/json; charset=utf-8");
-                PrintWriter out = resp.getWriter();
-                resp.setStatus(400);
-                ResponseDto<?> responseDto = new ResponseDto<>(-1, "패스워드가 틀렸습니다.", null);
-                ObjectMapper om2 = new ObjectMapper();
-                String body = om2.writeValueAsString(responseDto);
-                out.println(body);
-                out.flush();
-                return;
-            }
         }
+
+        // 패스워드 체크
+        User userPS = userOP.get();
+        SHA256 sh = new SHA256();
+        String encPassword = sh.encrypt(loginReqDto.getPassword());
+        if (!userPS.getPassword().equals(encPassword)) {
+            customResponse("패스워드가 틀렸습니다.", resp);
+            return;
+        }
+
+        // JWT토큰 생성 1초 = 1/1000
+        Date expire = new Date(System.currentTimeMillis() + (1000 * 60 * 60));
+
+        String jwtToken = JWT.create()
+                .withSubject("메타코딩")
+                .withExpiresAt(expire)
+                .withClaim("id", userPS.getId())
+                .withClaim("username", userPS.getUsername())
+                .sign(Algorithm.HMAC512("뺑소니"));
+        log.debug("디버그 : " + jwtToken);
+
+        // JWT토큰 응답
+        customJwtResponse(jwtToken, userPS, resp);
 
         // chain.doFilter(req, resp);
     }
 
-    private void denyNotPostRequest(HttpServletRequest req, HttpServletResponse resp)
+    private void customJwtResponse(String token, User userPS, HttpServletResponse resp)
             throws IOException, JsonProcessingException {
         resp.setContentType("application/json; charset=utf-8");
+        resp.setHeader("Authorization", "Bearer " + token);
         PrintWriter out = resp.getWriter();
-        if (!req.getMethod().equals("POST")) {
-            resp.setStatus(400);
-            ResponseDto<?> responseDto = new ResponseDto<>(-1, "로그인 시에는 post요청을 해야 합니다.", null);
-            ObjectMapper om = new ObjectMapper();
-            String body = om.writeValueAsString(responseDto);
-            out.println(body);
-            out.flush();
-            return;
-        }
+        resp.setStatus(200);
+        ResponseDto<?> responseDto = new ResponseDto<>(1, "성공", new SessionUser(userPS));
+        ObjectMapper om = new ObjectMapper();
+        String body = om.writeValueAsString(responseDto);
+        out.println(body);
+        out.flush();
+    }
+
+    private void customResponse(String msg, HttpServletResponse resp) throws IOException, JsonProcessingException {
+        resp.setContentType("application/json; charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        resp.setStatus(400);
+        ResponseDto<?> responseDto = new ResponseDto<>(-1, msg, null);
+        ObjectMapper om = new ObjectMapper();
+        String body = om.writeValueAsString(responseDto);
+        out.println(body);
+        out.flush();
     }
 
 }
